eb8ef93b4e8d8abc0893199a189513760ce7af46
jbusico <busicojulian@gmail.com>
2025-03-28 12:37:09 -0300

    Correccion de salida en pantalla

--- src/App.java
+++ src/App.java
@@ -3,8 +3,13 @@ public class App {
         int anioActual = 2025;
         Auto auto = new Auto("Ford", 2015, "Mustang", new Ruedas("Pirelli", 15, true), new Motor(false), new Tanque(20.5, 10.5, "Quantium"), "Rojo");
 
-        System.out.println(auto);
-        
+        System.out.println(auto.getColor());
+        System.out.println(auto.getMarca());
+        System.out.println(auto.getModelo());
+        System.out.println(auto.getMotor());
+        System.out.println(auto.getRuedas());
+        System.out.println(auto.getTanque());
+                
         /* System.out.println(anioActual);
         System.out.println(auto.getColor());
 
